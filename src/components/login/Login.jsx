9f801677045b0cef3a0b14f4887eb6af14bd4940
chiyoung <100222045+Returnmakdo@users.noreply.github.com>
2022-10-28 17:38:28 +0900

    회원가입,로그인 기능초기구현

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,5 +1,63 @@
-// 로그인 하는 컴포넌트 -> 회원가입 버튼 여기에 만들어서 누르면 회원가입 컴포넌트 렌더링
+import { useForm } from "react-hook-form";
+import React from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+// import useCookie from "react-cookie";
+
 function Login() {
-  return <div>로그인 하는 컴포넌트</div>;
+  const { register, handleSubmit } = useForm();
+  const navigate = useNavigate();
+
+  const signin = (data) => {
+    axios
+      .post("", data)
+      .then((res) => {
+        console.log(res);
+        const accessToken = res.data.accessToken.split(" ")[1];
+        const refreshToken = res.data.refreshToken.split(" ")[1];
+        localStorage.setItem("accessToken", accessToken);
+        localStorage.setItem("refreshToken", refreshToken); // setCookie 해야함 !!
+        if (res.data.message === "로그인되었습니다.") {
+          navigate("/HomeP");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        if (error.code === "ERR_BAD_REQUEST") {
+          Swal.fire({
+            icon: "error",
+            title: "다시 확인해주세요!",
+            text: "아이디 또는 비밀번호가 틀렸습니다.",
+          });
+        }
+      });
+  };
+  return (
+    <Stbox>
+      <StSignin>
+        <StForm as="form">
+          <StLogin>
+            <input label="아이디" variant="outlined" type="text" {...register("username")} />
+            <input label="비밀번호" variant="outlined" type="password" {...register("password")} />
+          </StLogin>
+          <Button variant="contained" type="submit" onClick={handleSubmit(signin)}>
+            로그인
+          </Button>
+        </StForm>
+      </StSignin>
+    </Stbox>
+  );
 }
 export default Login;
+
+const StForm = styled.form``;
+
+/*전체 박스*/
+const Stbox = styled.div``;
+
+/*로그인박스*/
+const StLogin = styled.div``;
+
+/*전체 로그인박스*/
+const StSignin = styled.div``;
--- src/components/signup/SignUp.jsx
+++ src/components/signup/SignUp.jsx
@@ -1,5 +1,160 @@
-// 회원가입 컴포넌트
+import styled from "styled-components";
+
+import { useForm } from "react-hook-form";
+import React from "react";
+import axios from "axios";
+
 function SignUp() {
-  return <div>회원가입 하는 컴포넌트</div>;
+  // 회원가입 데이터 값
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+  } = useForm();
+
+  // 비밀번호 입력값 추적
+  const password = watch("password");
+
+  //회원가입 데이터 전송
+  const onSubmit = (data) => {
+    axios
+      .post("", data)
+      .then((res) => {
+        if (res.data.message === "SUCCESS") {
+          alert("회원가입에 성공했습니다.");
+          window.location.replace("/");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        if (error.code === "ERR_BAD_REQUEST") {
+          alert("중복된 아이디 또는 닉네임입니다. 중복 검사를 진행해주세요.");
+        }
+      });
+  };
+
+  //닉네임 중복검사 진행하기
+  const nickDup = () => {
+    const nickname = watch("nickname");
+    axios.post("", { value: nickname }).then((res) => {
+      if (res.data.result === true) {
+        alert("중복된 닉네임 입니다.");
+      } else {
+        alert("사용 가능한 닉네임 입니다.");
+      }
+    });
+  };
+
+  //아이디 중복검사 진행하기
+  const userDup = () => {
+    const username = watch("username");
+    axios.post("", { value: username }).then((res) => {
+      if (res.data.result === true) {
+        alert("중복된 아이디 입니다.");
+      } else {
+        alert("사용 가능한 아이디 입니다.");
+      }
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input
+        autoFocus
+        margin="dense"
+        label="닉네임"
+        type="text"
+        fullWidth
+        variant="standard"
+        placeholder="8자 이내로 작성해주세요."
+        required
+        error={errors?.nickname}
+        helperText={errors.nickname?.message}
+        {...register("nickname", {
+          maxLength: {
+            value: 8,
+            message: "8글자 이내로 작성해주세요",
+          },
+        })}
+      />
+      <StBtn onClick={nickDup}>닉네임 검사</StBtn>
+      <input
+        autoFocus
+        margin="dense"
+        label="아이디"
+        type="text"
+        fullWidth
+        variant="standard"
+        placeholder="2~10자로 영문을 포함하고 숫자, 특수문자는_만 사용해주세요."
+        required
+        error={errors?.username}
+        helperText={errors.username?.message}
+        {...register("username", {
+          maxLength: {
+            value: 10,
+            message: "10글자 이하로 작성해주세요",
+          },
+          minLength: {
+            value: 4,
+            message: "4글자 이상으로 작성해주세요",
+          },
+          pattern: {
+            value: /^(?=.*[a-zA-Z])[a-zA-Z0-9_]{2,10}$/,
+            message: "형식에 맞지 않는 아이디 입니다.",
+          },
+        })}
+      />
+      <StBtn onClick={userDup}>아이디 검사</StBtn>
+      <input
+        autoFocus
+        margin="dense"
+        label="비밀번호"
+        type="password"
+        fullWidth
+        variant="standard"
+        placeholder="4~20 자리 영문을 포함하고 숫자, 특수문자(!@#$%^&*)만 사용해주세요."
+        required
+        error={errors?.password}
+        helperText={errors.password?.message}
+        {...register("password", {
+          maxLength: {
+            value: 20,
+            message: "20자리 이하로 작성해주세요",
+          },
+          minLength: {
+            value: 4,
+            message: "4자리 이상으로 작성해주세요",
+          },
+          pattern: {
+            value: /^(?=.*[a-zA-Z])[0-9a-zA-Z!@#$%^&*]{4,20}$/,
+            message: "형식에 맞지 않는 비밀번호 입니다.",
+          },
+        })}
+      />
+      <input
+        autoFocus
+        margin="dense"
+        label="비밀번호 재확인"
+        type="password"
+        fullWidth
+        variant="standard"
+        placeholder="비밀번호를 재입력해주세요."
+        required
+        error={errors?.confirm}
+        helperText={errors.confirm?.message}
+        {...register("confirm", {
+          validate: {
+            confirmPw: (v) => v === password || "비밀번호가 일치하지 않습니다.",
+          },
+        })}
+      />
+      <Button type="submit">가입하기</Button>
+    </form>
+  );
 }
 export default SignUp;
+
+const StBtn = styled(Button)`
+  width: 100px;
+`;
